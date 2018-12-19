b6be6016b77928a2e6b4680d24f60f2aba1af670
Alexandre Sagette <sagette.alexandre@gmail.com>
2018-12-19 15:26:04 +0100

    fix translation

--- src/highlightediting.js
+++ src/highlightediting.js
@@ -95,7 +95,7 @@ export default class HighlightEditing extends Plugin {
 				{
 					model: 'bluePen',
 					class: 'pen-blue',
-					title: 'Pink pen',
+					title: 'Blue pen',
 					color: 'var(--ck-highlight-pen-blue)',
 					type: 'pen'
 				}
